8109a7cc4242ff78c55604256246058982e6362e
hakimamarullah <hakimamarullah@gmail.com>
2025-06-21 16:31:40 +0700

    [FIX] Fix ItemInfo deserializing issue (add no arg and all arg constructor)

--- src/main/java/com/sg/obs/dto/item/ItemInfo.java
+++ src/main/java/com/sg/obs/dto/item/ItemInfo.java
@@ -1,13 +1,17 @@
 package com.sg.obs.dto.item;
 
 import com.sg.obs.dto.BaseInfo;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
 import lombok.experimental.SuperBuilder;
 
 
 @Data
 @SuperBuilder(toBuilder = true)
+@NoArgsConstructor
+@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 public class ItemInfo extends BaseInfo {
 
